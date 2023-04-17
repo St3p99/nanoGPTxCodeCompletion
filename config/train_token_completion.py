28231adb410af56b9fb2cb24dd99976fecdd0f71
Stefano <pernastefano99@gmail.com>
2023-04-17 14:18:16 +0200

    edit config

--- config/train_token_completion.py
+++ config/train_token_completion.py
@@ -22,18 +22,18 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 2000
+lr_decay_iters = 2000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
 
 # DDP settings
-backend = 'gloo' # 'nccl', 'gloo', etc.
+backend = 'nccl' # 'nccl', 'gloo', etc.
 # system
 device = 'cuda:0' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
-dtype = 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+dtype = 'bfloat16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
 compile = False # use PyTorch 2.0 to compile the model to be faster
 
 
